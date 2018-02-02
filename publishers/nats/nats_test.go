5e1155f5e806f6dd57a6b6adf407bbbaa71c37e8
Alexander Ewetumo <trinoxf@gmail.com>
2018-02-02 15:03:55 +0100

    Fix TestNATStreaming

--- publishers/nats/nats_test.go
+++ publishers/nats/nats_test.go
@@ -6,8 +6,6 @@ import (
 	"github.com/gokit/cqrskit"
 	"github.com/influx6/faux/tests"
 
-	"github.com/nats-io/go-nats"
-
 	pubnats "github.com/gokit/cqrskit/publishers/nats"
 )
 
@@ -28,7 +26,7 @@ func TestNATSPublisher(t *testing.T) {
 
 func TestNATStreamingPublisher(t *testing.T) {
 	//t.Skip("Need to setup nats server")
-	publisher := pubnats.NewNATStreamingPublisher("test-daddy", "test-child", nats.DefaultURL, cqrskit.JSONEncoder{})
+	publisher := pubnats.NewNATStreamingPublisher("test-daddy", "test-child", defaultURL, cqrskit.JSONEncoder{})
 	defer publisher.Close()
 
 	if err := publisher.Publish("users.events", cqrskit.EventCommit{}, func(ack cqrskit.PubAck) {}); err != nil {
